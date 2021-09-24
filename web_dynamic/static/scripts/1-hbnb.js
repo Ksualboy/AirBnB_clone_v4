e10147201839360dcfd885e268dd78c0c015bd11
Diego Varela <2806@holbertonschool.com>
2021-09-24 10:21:05 -0300

    Testing Task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,14 @@
 window.onload = function() {
     let amenities = {}; //dictionary
+    
 
-    $( "li input" ).on( "click", function() {
-        if ($("input[type=checkbox]").prop(
-            ":checked")) {
-              alert($("li").prop("data-name") + " was checked");
-          } else {
-              alert($("li").prop("data-name") + " was unchecked");
-          }
+    
+
+    $( '.popper li input').on( 'click', function() {
+        if ($('input[type=checkbox]').prop(':checked')) {
+              alert($('.popper li').attr('data-name') + ' was checked');
+        } else {
+              alert($('.popper li').attr('data-name') + ' was unchecked');
+        }
       });
 };
