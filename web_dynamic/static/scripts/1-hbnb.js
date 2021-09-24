465439404eeac068a569867258e75ace524e80ed
Diego Varela <2806@holbertonschool.com>
2021-09-24 10:13:02 -0300

    Testing task 2
    Co-authored-by: Ksualboy <Ksualboy@users.noreply.github.com>

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,12 @@
 window.onload = function() {
-    
+    let amenities = {}; //dictionary
+
+    $( "li input" ).on( "click", function() {
+        if ($("input[type=checkbox]").prop(
+            ":checked")) {
+              alert($("li").prop("data-name") + " was checked");
+          } else {
+              alert($("li").prop("data-name") + " was unchecked");
+          }
+      });
 };
